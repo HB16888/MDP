fd6e438f6cf68eee0adf76e8c61569c5463a2249
HB16888 <u202114687@hust.edu.cn>
2024-07-15 11:52:57 +0800

    chore: Update submodule for stable-diffusion

--- lib/models/monodetr/depthaware_transformer.py
+++ lib/models/monodetr/depthaware_transformer.py
@@ -464,13 +464,13 @@ class DepthAwareDecoderLayer(nn.Module):
         # self attention
         q = k = self.with_pos_embed(tgt, query_pos)
         
-        # q_content = self.sa_qcontent_proj(q)
-        # q_pos = self.sa_qpos_proj(q)
-        # k_content = self.sa_kcontent_proj(k)
-        # k_pos = self.sa_kpos_proj(k)
-        # v = self.sa_v_proj(tgt)
-        # q = q_content + q_pos
-        # k = k_content + k_pos
+        q_content = self.sa_qcontent_proj(q)
+        q_pos = self.sa_qpos_proj(q)
+        k_content = self.sa_kcontent_proj(k)
+        k_pos = self.sa_kpos_proj(k)
+        v = self.sa_v_proj(tgt)
+        q = q_content + q_pos
+        k = k_content + k_pos
         
         q = q.transpose(0, 1)
         k = k.transpose(0, 1)
