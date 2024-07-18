556e298f514d77b1a644ff362ab521d2b2e9922a
HB16888 <u202114687@hust.edu.cn>
2024-07-18 17:31:49 +0800

    chore: Update optimizer_helper.py to include unet weights and biases in build_optimizer function

--- lib/helpers/optimizer_helper.py
+++ lib/helpers/optimizer_helper.py
@@ -5,15 +5,21 @@ from torch.optim.optimizer import Optimizer
 
 
 def build_optimizer(cfg_optimizer, model):
-    weights, biases = [], []
+    weights, biases ,unet_biases,unet_weights= [], [],[],[]
     for name, param in model.named_parameters():
-        if 'bias' in name:
+        if 'bias' in name and "unet" not in name:
             biases += [param]
-        else:
+        elif 'bias' not in name and "unet" not in name:
             weights += [param]
+        elif 'bias' in name and "unet" in name:
+            unet_biases += [param]
+        else:
+            unet_weights += [param]
 
     parameters = [{'params': biases, 'weight_decay': 0},
-                  {'params': weights, 'weight_decay': cfg_optimizer['weight_decay']}]
+                  {'params': weights, 'weight_decay': cfg_optimizer['weight_decay']},
+                  {'params': unet_biases, 'weight_decay': 0,"lr":cfg_optimizer['lr']/10},
+                  {'params': unet_weights, 'weight_decay': cfg_optimizer['weight_decay'],"lr":cfg_optimizer['lr']/10}]
 
     if cfg_optimizer['type'] == 'sgd':
         optimizer = optim.SGD(parameters, lr=cfg_optimizer['lr'], momentum=0.9)
